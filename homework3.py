ddc545dc9fcb96284764f66b2e52121feb986f7d
BezborodovVV37 <blood371993@gmail.com>
2024-07-30 14:51:45 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,11 @@
+name = "Vasya"
+print(name, type (name))
+print("Name "+ name)
+age=  30
+print(age, type (age))
+new_age=1
+print(age+new_age)
+print(age + new_age, type(age+new_age))
+is_student= True
+print(is_student, type (is_student))
+
